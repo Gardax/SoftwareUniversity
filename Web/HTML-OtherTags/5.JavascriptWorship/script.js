a1df650eadd06acfc7fb75db2bf90b7eb99aeb8e
Georgi <gardax95@gmail.com>
2014-06-23 01:10:46 +0300

    Added HTML Tables and CSS Overview homeworks.

---
+++ Web/HTML-OtherTags/5.JavascriptWorship/script.js
@@ -0,0 +1,9 @@
+var ctx = document.getElementById("myCanvas").getContext("2d");
+ctx.font = "bold 27px tahoma";
+ctx.textAlign = "center";
+ctx.fillText("I love JavaScript", 135, 50);
+ctx.fillStyle = "#F1DA4E";
+ctx.fillRect(67.5, 80, 135, 135);
+ctx.font = "bold 85px segoe UI";
+ctx.fillStyle = "#2F312D";
+ctx.fillText("JS", 150, 205);
